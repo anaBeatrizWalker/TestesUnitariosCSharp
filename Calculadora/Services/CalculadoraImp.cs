98b31f22bbf6067b34210fdf8535e1300abcc83b
anaBeatrizWalker <anabiawalker@gmail.com>
2024-09-25 14:27:11 -0300

    Adicionando classe de implementação da calculadora

---
+++ Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    //Implementação da calculadora
+    public class CalculadoraImp
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
